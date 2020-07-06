38db56907ed3f3a0c502488e75feb335c6c4e213
FSorrentinoC <sorrentino@live.com>
2020-07-06 10:39:43 -0300

    First class: Positon

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,8 +12,8 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("hello word");
-		
+		Position pos = new Position (3, 5);
+		System.out.println(pos);
 		
 		
 		
